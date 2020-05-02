9218fafb855b90e0461cb700c91cb1372509ad6e
Ashish <ashishpr78@gmail.com>
2020-05-03 01:19:59 +0530

    "Mongo db added and Connection Established"

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express= require('express');
-
+const mongoose = require('mongoose');
 const app = express();
-app.set('view engine','ejs');
-app.get("/profile/:name",function(req,res){
-    console.log(req.params.name);
-    data={email:'ashish@email.com',name:'ashish',address:'noida',skills:['nodejs','php','java']}; 
-    res.render('home',{name:req.params.name,data:data});
-});
 
-app.listen(5000);
+mongoose.connect('mongodb+srv://Ashish:uW0SCYDZrb7Qk7iA@ashishcluster-8hrzy.mongodb.net/test?retryWrites=true&w=majority',
+{useNewUrlParser:true
+,
+useUnifiedTopology:true
+}).then(()=>{console.warn("db connection done")});
