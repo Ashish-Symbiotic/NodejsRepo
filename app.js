1b6ac4cb25a7ea05ff685b4b3f2c31ac262c41fb
Ashish <ashishpr78@gmail.com>
2020-05-03 00:41:48 +0530

    "Adding ejs"

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const express=require('express');
-const app=express();
-const checkUrl = require('./middleware');
-const router=express.Router();
+const express= require('express');
 
-//app.use(checkUrl);
-app.get('/',function(req,res){
-   // res.send("Hello Express.js");
-    res.sendFile(__dirname+"/home.html");
+const app = express();
+app.set('view engine','ejs');
+app.get("/profile/:name",function(req,res){
+    console.log(req.params.name);
+    data={email:'ashish@email.com',name:'ashish',address:'noida',skills:['nodejs','php','java']}; 
+    res.render('home',{name:req.params.name,data:data});
 });
-router.get('/about',checkUrl,function(req,res){
-   // res.send("Hello Express.js this is about page")
-   res.sendFile(__dirname+"/about.html");
-});
-app.post('/login',function(req,res){
-    //res.send("Hello Express.js this is login page")
-    res.sendFile(__dirname+"/login.html");
-});
-app.put('/check',function(req,res){
-    //res.send("Hello Express.js this is put page")
-    res.sendFile(__dirname+"/check.html");
-});
-app.delete('/logout',function(req,res){
-    //res.send("Hello Express.js this is delete page")
-    res.sendFile(__dirname+"/about.html");
-});
-app.use("/",router);
+
 app.listen(5000);
