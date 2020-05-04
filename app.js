d628ec6c7e1eb8e6261b32092e30c093df53fbe6
Ashish <ashishpr78@gmail.com>
2020-05-04 12:20:42 +0530

    "Data Inserted in Mongo db"

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express= require('express');
 const mongoose = require('mongoose');
 const app = express();
-const user = require('./users');
+const user = require('./models/users');
 mongoose.connect('mongodb+srv://Ashish:uW0SCYDZrb7Qk7iA@ashishcluster-8hrzy.mongodb.net/tutorialdemo?retryWrites=true&w=majority',
 {useNewUrlParser:true,
 useUnifiedTopology:true
 }).then(()=>{console.warn("db connection done")});
-user.find({},function(err,users){
+/*user.find({},function(err,users){
     if(err)
     {
         console.warn("There is an error"+err);
@@ -15,4 +15,11 @@ user.find({},function(err,users){
     {
         console.warn(users);
     }
-})
+})*/
+const data = new user({
+    _id:new mongoose.Types.ObjectId(),
+    name:"Buttons",
+    email:"buttons@mail.com",
+    phone:"9856211478"
+});
+data.save().then((result)=>{console.warn("result")}).catch(err=>console.log(err));
--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ let userSchema = new mongoose.Schema({
     //_id:mongoose.Schema.Types.ObjectId(),
     name:String,
     phone:String,
-    address:String
+    email:String
 
 });
 
