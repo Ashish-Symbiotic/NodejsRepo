8903f7c8adc90fcef5d7d5600f2e3a6ea2bed791
Ashish <ashishpr78@gmail.com>
2020-05-08 12:33:55 +0530

    "used middleware in app.js and used post method"

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const mongoose = require('mongoose');
 const app = express();
+app.set('view engine','ejs');
+var bodyParser= require('body-parser');
+var encoder=bodyParser.urlencoded({extended:true});
 const user = require('./models/users');
 mongoose.connect('mongodb+srv://Ashish:uW0SCYDZrb7Qk7iA@ashishcluster-8hrzy.mongodb.net/tutorialdemo?retryWrites=true&w=majority',
 {useNewUrlParser:true,
@@ -16,10 +19,29 @@ useUnifiedTopology:true
         console.warn(users);
     }
 })*/
-const data = new user({
+/*const data = new user({
     _id:new mongoose.Types.ObjectId(),
     name:"Buttons",
     email:"buttons@mail.com",
     phone:"9856211478"
+});*/
+//data.save().then((result)=>{console.warn("result")}).catch(err=>console.log(err));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/profile:name',function(req,res ){
+    data1={email:'ashish@email.com',name:"Ashish",address:"Ashish Address",skills:['nodejs','sql','php','js','RPA']};
+        console.log(req.params.name);
+    res.render('home',{name:req.params.name,data:data1});
+
 });
-data.save().then((result)=>{console.warn("result")}).catch(err=>console.log(err));
+//second parameter in get or post req act as a middleware
+app.post('/login',encoder,function(req,res){
+   
+    res.render('home');
+});
+app.get('/login',function(req,res){
+    console.log(req.query);
+    res.render('login');
+});
+app.listen(5000,function(){
+console.log("Running....");
+});
